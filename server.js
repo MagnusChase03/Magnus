41b89f966ab9bae8e1cdcdd21e7bca78c4d845dd
Zachary Ness <magnuschase03@pop-os.localdomain>
2021-09-06 11:19:54 -0500

    Added YoutubeMusicDownloader page

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const formidable = require("express-formidable");
 
 const app = express();
 app.use(express.static(__dirname + "/frontEnd"));
+app.use(formidable());
 
 app.get("/", (req, res) => {
 
     res.sendFile(__dirname + "/frontEnd/index.html");
+    
+});
+
+app.get("/YoutubeMusicDownloader", (req, res) => {
+
+    res.sendFile(__dirname + "/frontEnd/YoutubeMusicDownloader.html");
+    
+});
+
+app.post("/YoutubeMusicDownloaderPost", (req, res) => {
+
+    const URL = req.fields["URL"];
+    const fileName = req.fields["fileName"];
+
+    console.log("Attempt at " + URL);
     
 });
 
