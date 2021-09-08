b8636da2d4f33f8dd1fe2e19fbc29f47e93f791d
Zachary Ness <magnuschase03@pop-os.localdomain>
2021-09-08 16:04:19 -0500

    Added a simple equation to determin if professr is good

--- backEnd/ProfessorSearch.py
+++ backEnd/ProfessorSearch.py
@@ -8,5 +8,7 @@ school = ratemyprofessor.get_schools_by_name("University of Texas at Dallas")[0]
 professor = ratemyprofessor.get_professors_by_school_and_name(school, professorName)[(int) (index)]
 
 recentComment = professor.get_ratings()[0].comment
+score = 2 * professor.rating - professor.difficulty
+isGood = score > 3
 
-print("Professor: %s (%s)/Rating: %s/Difficulty: %s/Recent Comment: %s" % (professor.name, professor.department, professor.rating, professor.difficulty, recentComment))
+print("Professor: %s (%s)/Rating: %s/Difficulty: %s/Good Overall: %s/Recent Comment: %s" % (professor.name, professor.department, professor.rating, professor.difficulty, isGood, recentComment))
