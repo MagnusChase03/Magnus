39ca9d120cdf6609d67e90c1a7af68d1b79b5b4d
Zachary <zachness2003@gmail.com>
2022-02-01 18:59:19 -0600

    Basic frontpage

---
+++ magnus/src/components/App.js
@@ -0,0 +1,35 @@
+import NavBar from "./NavBar";
+
+function App() {
+
+    return (
+
+        <div className="App">
+
+            <NavBar />
+
+            <div className="container-fluid">
+
+                <div className="row">
+
+                    <div className="col-sm-1"></div>
+
+                    <div className="col-sm-10 content">
+
+                        <h1>Hello World!</h1>
+
+                    </div>
+
+                    <div className="col-sm-1"></div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default App;
--- magnus/src/index.js
+++ magnus/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import './styles/custom.css';
+import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
